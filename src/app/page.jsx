9edb6cfd58391501ebf7df080adfba4745f56a34
cs6662128 <s6604062662128@email.kmutnb.ac.th>
2025-09-22 21:08:13 +0700

    home updated

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -3,14 +3,18 @@ import Link from "next/link";
 import Dashboard from "./dashboard/page";
 import Nav from "./nav/page";
 import Act from "./act/page";
+import History from "./history/page";
+import Detail from "./detail/page";
 
 export default function Home() {
   return (
    <div className={'flex '}>
     
     <Nav></Nav>
-    <Dashboard></Dashboard>
+    {/* <Dashboard></Dashboard> */}
     {/* <Act></Act> */}
+    {/* <History></History> */}
+    <Detail></Detail>
     
     
 
